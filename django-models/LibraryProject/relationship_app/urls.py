6e1987973161a40864bd59d2bc9e8e4f878a9010
mogaya <mogayainnocent@gmail.com>
2024-11-15 22:51:25 +0300

    3. Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,5 +12,9 @@ urlpatterns = [
     # path('logout/', views.LogoutView.as_view(), name='logout'),
     path('register/', views.register.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name= 'relationship_app/login.html'), name='login' ),
-    path('logout/', LogoutView.as_view(template_name= 'relationship_app.logout.html'), name='logout')
+    path('logout/', LogoutView.as_view(template_name= 'relationship_app.logout.html'), name='logout'),
+
+    path('admin/', views.admin_view, name ='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import View
 
+from django.contrib.auth.decorators import user_passes_test
+
 from .models import Book
 from .models import Library
 
@@ -50,3 +52,25 @@ class register(View):
             login(request, user)
             return redirect('login')
         return render(request, 'relationship_app/register.html', {'form': form})
+    
+# Role Based Views
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_views.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
