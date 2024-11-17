df0fb09e35c8dcf288853d0cf0f07505bd8c8ce8
mogaya <mogayainnocent@gmail.com>
2024-11-17 15:58:51 +0300

    4. Implementing Custom Permissions in Django-crr1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -18,5 +18,6 @@ urlpatterns = [
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
 
-    
+    path('add_book/', views.add_book, name='add_book'),  # URL for adding a book
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
 ]
