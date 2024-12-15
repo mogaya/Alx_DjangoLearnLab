1b839447318689e9ec25f6eb7f701f103ec050e4
mogaya <mogayainnocent@gmail.com>
2024-12-15 19:16:20 +0300

    2. Implementing User Follows and Feed Functionality crr3

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -32,6 +32,6 @@ class FeedView(APIView):
 
     def get(self, request):
         followed_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
