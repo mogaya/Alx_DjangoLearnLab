6955b2730d92966513cf771f9fdb884d0cd22211
mogaya <mogayainnocent@gmail.com>
2024-12-15 10:53:56 +0300

    0. Project Setup and User Authentication for a Social Media API crr

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,15 +1,37 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 from .models import CustomUser
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
+
+# class RegisterSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = CustomUser
+#         fields = ['username', 'password', 'email', 'bio', 'profile_picture']
+#         extra_kwargs = {'password': {'write_only': True}}
+
+#     def create(self, validated_data):
+#         user = CustomUser.objects.create_user(**validated_data)
+#         return user
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = ['username', 'password', 'email', 'bio', 'profile_picture']
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
+        # Create the user
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', ''),
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        # Create a token for the user
+        Token.objects.create(user=user)
         return user
     
 class LoginSerializer(serializers.Serializer):
