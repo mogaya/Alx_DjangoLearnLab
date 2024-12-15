13efd56a3da292f0d23d40587df2119828c4d4ef
mogaya <mogayainnocent@gmail.com>
2024-12-15 19:06:55 +0300

    2. Implementing User Follows and Feed Functionality crr1

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
+# from rest_framework.views import ApiView
 from rest_framework.response import Response
-from rest_framework import status, permissions
+from rest_framework import status, permissions, generics
 from rest_framework.authtoken.models import Token
 from .serializers import RegisterSerializer, LoginSerializer
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ObjectDoesNotExist
 
-User = get_user_model()
+CustomUser = get_user_model()
 
 # Create your views here.
-class RegisterView(APIView):
+class RegisterView(generics.GenericAPIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
@@ -19,7 +19,7 @@ class RegisterView(APIView):
             return Response({"token": token.key}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-class LoginView(APIView):
+class LoginView(generics.GenericAPIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
@@ -30,12 +30,12 @@ class LoginView(APIView):
     
 
 # Follow Management Views
-class FollowUserView(APIView):
+class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
         try:
-            target_user = User.objects.get(id=user_id)
+            target_user = CustomUser.objects.get(id=user_id)
             if request.user == target_user:
                 return Response({"detail": "You can not follow yourself"}, status=status.HTTP_400_BAD_REQUEST)
             if request.user.is_following(target_user):
@@ -43,17 +43,17 @@ class FollowUserView(APIView):
             request.user.follow(target_user)
             return Response({"detail": "Followed Successfully"}, status=status.HTTP_200_OK)
         except ObjectDoesNotExist:
-            return Response({"detail": "User not found"}, status=status.HTTP_403_FORBIDDEN)
+            return Response({"detail": "CustomUser not found"}, status=status.HTTP_403_FORBIDDEN)
 
-class UnfollowUserView(APIView):
+class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
         try:
-            target_user = User.objects.get(id=user_id)
+            target_user = CustomUser.objects.get(id=user_id)
             if not request.user.is_following(target_user):
                 return Response({"detail": "You are not following this user"}, status=status.HTTP_400_BAD_REQUEST)
             request.user.unfollow(target_user)
             return Response({"detail": "Unfollowed Successfully"}, status=status.HTTP_200_OK)
         except ObjectDoesNotExist:
-            return Response({"detail": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"detail": "CustomUser not found"}, status=status.HTTP_404_NOT_FOUND)
